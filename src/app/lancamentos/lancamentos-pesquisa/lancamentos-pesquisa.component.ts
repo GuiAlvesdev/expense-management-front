cafca7a741cfde20ee6e84c2ac7e604099afc59e
GuiAlvesdev <guialves1353@gmail.com>
2022-12-11 12:07:02 -0300

    initial component service :construction:

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LancamentoService } from '../lancamento.service';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
   templateUrl: './lancamentos-pesquisa.component.html',
   styleUrls: ['./lancamentos-pesquisa.component.css']
 })
-export class LancamentosPesquisaComponent {
+export class LancamentosPesquisaComponent implements OnInit{
 
-  
+  lancamentos: any[] = [] ;
+
+
+  constructor(private lancamentoService: LancamentoService) {}
+
+  ngOnInit(): void {
+    this.pesquisar();
+  }
+
+  pesquisar(): void {
+    this.lancamentoService.pesquisar()
+      .then(lancamentos => this.lancamentos = lancamentos);
+  }
 
 }
--- src/app/lancamentos/lancamentos.module.ts
+++ src/app/lancamentos/lancamentos.module.ts
@@ -14,6 +14,7 @@ import { SelectButtonModule } from 'primeng/selectbutton';
 import { TooltipModule } from 'primeng/tooltip';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { SharedModule } from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -25,6 +26,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     FormsModule,
+    HttpClientModule,
 
     SharedModule,
 
