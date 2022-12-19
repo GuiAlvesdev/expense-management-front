2f9191bbd89885492b66c657ebd7d160e3be111b
GuiAlvesdev <guialves1353@gmail.com>
2022-12-19 14:04:38 -0300

    refactor code :recycle:

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://easy-financas.azurewebsites.net',
-  tokenAllowedDomains: [ /easy-financas.azurewebsites.net/ ],
-  tokenDisallowedRoutes: [/\/oauth\/token/],
+  tokenAllowedDomains: [ 'easy-financas.azurewebsites.net' ],
+  tokenDisallowedRoutes: ['/oauth\/token/'],
 };
